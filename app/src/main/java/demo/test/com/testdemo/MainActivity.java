d071936d79b63136a82c49f9a2e1878153b723f1
XinyueZhao <zhao@eosnewmedia.de>
2014-11-05 16:30:01 +0100

    Added first test case.

--- app/src/main/java/demo/test/com/testdemo/MainActivity.java
+++ app/src/main/java/demo/test/com/testdemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(LAYOUT);
 	}
 
 
@@ -36,4 +36,6 @@ public class MainActivity extends Activity {
 
 		return super.onOptionsItemSelected(item);
 	}
+
+	public static final int LAYOUT = R.layout.activity_main;
 }
